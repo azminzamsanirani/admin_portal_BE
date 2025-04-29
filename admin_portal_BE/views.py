f2fe34507e13bc2fcb12a3c077b9a6c195534977
Azmin Zamsanirani <azamsanirani@Lenovo.com>
2025-04-29 16:44:25 +0800

    initial setup

---
+++ admin_portal_BE/views.py
@@ -0,0 +1,23 @@
+from django.contrib.auth.models import Group, User
+from rest_framework import permissions, viewsets
+from rest_framework.permissions import AllowAny
+
+from admin_portal_BE.serializers import GroupSerializer, UserSerializer
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = User.objects.all().order_by('-date_joined')
+    serializer_class = UserSerializer
+    permission_classes = [AllowAny]
+
+
+class GroupViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows groups to be viewed or edited.
+    """
+    queryset = Group.objects.all().order_by('name')
+    serializer_class = GroupSerializer
+    permission_classes = [permissions.IsAuthenticated]
